4867caf3ed5108adcf6ce190278f698bfbd523ec
Udit Shahi <udhishahi1606@gmail.com>
2025-02-27 02:14:04 +0530

    The initial gateway setup done

---
+++ src/test/java/com/service/geteway/ServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.geteway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
