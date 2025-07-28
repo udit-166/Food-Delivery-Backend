2e1db4065732943037571e6be48d89ba85a2043d
Udit Shahi <114797354+udit-166@users.noreply.github.com>
2025-07-28 18:42:10 +0530

    modify register user api

--- src/main/java/com/auth/user/core/controller/AuthController.java
+++ src/main/java/com/auth/user/core/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -112,7 +113,7 @@ public class AuthController {
 	}
 	
 	@PostMapping(AppConstants.REGISTER_USER)
-	public ResponseEntity<LoginResponse> registerUser(@RequestParam User user){
+	public ResponseEntity<LoginResponse> registerUser(@RequestBody User user){
 		try {
 			LoginResponse response  =  new LoginResponse();
 			UserDto existUser = authService.login(user.getPhone());
