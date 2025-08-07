9880c3acd3b4cbcbeec1f8caa25a55fb96d4aa0d
Udit Shahi <114797354+udit-166@users.noreply.github.com>
2025-08-07 18:31:37 +0530

    Update AppConstant.java

--- src/main/java/com/notification/send/adapter/constant/AppConstant.java
+++ src/main/java/com/notification/send/adapter/constant/AppConstant.java
@@ -12,13 +12,8 @@ public class AppConstant {
 	
 	public static final long OTP_EXPIRY_MINUTES = 5;
 	
-	public static final String TWILIO_API_KEY = "EmVTwn5bucyZ7rQkp2aNC3IYLxeGSBXRhi6qzlDP8d1HjUf4AtxpQAvlgukrX1FBUSwqfaWiPmLs5heD";
 	
 	public static final String TWILIO_SEND_ID = "FSTSMS";
-
-	public static final String TWILIO_SID_ACCOUNT = "AC2c077cec135ee4bb5ca4a3a6f2296e78";
-	
-	public static final String TWILIO_AUTH_TOKEN = "8aa02bad8054bfeab3ce986f8e1ae614";
 	
 	public static final String TWILIO_VERIFY_SID_ACCOUNT = "VA7fd18c315b405571565ddeb63d1cf1b0";
 	public static final String NOTIFICATION_CONTROLLER = "/api/notification";
