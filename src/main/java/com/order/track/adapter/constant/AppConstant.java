6e2fc787d9f45a3f6091b31839f009d1a49772a4
Udit Shahi <114797354+udit-166@users.noreply.github.com>
2025-08-14 16:36:21 +0530

    Update AppConstant.java

--- src/main/java/com/order/track/adapter/constant/AppConstant.java
+++ src/main/java/com/order/track/adapter/constant/AppConstant.java
@@ -29,9 +29,9 @@ public class AppConstant {
 	
 	public static final String ORDER_SUMMARY = "/order_summary";
 	
-	public static final String RAZORPAY_SECRET = "ciupxXQlukLrJkY27JXohzE7";
+	public static final String RAZORPAY_SECRET = "";
 	
-	public static final String RAZORPAY_KEY = "rzp_test_3aaBFUOTbkYqzS";
+	public static final String RAZORPAY_KEY = "";
 	
 	public static final String GET_COUNT_OF_ORDER_BY_CUSTOMER_ID = "/getCountOfOrderByCustomerId/{customer_id}";
 	
