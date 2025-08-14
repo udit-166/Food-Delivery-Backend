e25d7be69ee48014bb017003586b42e413b0f8bd
Udit Shahi <114797354+udit-166@users.noreply.github.com>
2025-08-14 16:40:48 +0530

    Update AppConstant.java

--- src/main/java/com/notification/send/adapter/constant/AppConstant.java
+++ src/main/java/com/notification/send/adapter/constant/AppConstant.java
@@ -4,24 +4,24 @@ public class AppConstant {
 
 	public static final String EMAIL_NOTIFICATION="email_notification";
 	
-	public static final String EMAIL_ADDRESS = "uditshahi96@gmail.com";
+	public static final String EMAIL_ADDRESS = "";
 	
-	public static final String EMAIL_PASSWORD = "ddrpyglpsxptbbto";
+	public static final String EMAIL_PASSWORD = "";
 	
-	public static final String CLIENT_ID = "326597451983-4heq0h4d94pcnvku860ie4bfjmdfgtag.apps.googleusercontent.com";
+	public static final String CLIENT_ID = "";
 	
 	public static final long OTP_EXPIRY_MINUTES = 5;
 	
 	
-	public static final String TWILIO_SEND_ID = "FSTSMS";
+	public static final String TWILIO_SEND_ID = "";
 	
-	public static final String TWILIO_VERIFY_SID_ACCOUNT = "VA7fd18c315b405571565ddeb63d1cf1b0";
+	public static final String TWILIO_VERIFY_SID_ACCOUNT = "";
 	public static final String NOTIFICATION_CONTROLLER = "/api/notification";
 	
 	public static final String SEND_EMAIL_NOTIFICATION = "/sendNotification";
 	
-	public static final String FIREBASE_API_KEY = "AIzaSyCQlHRqHk69iam0SaRviwse3AyAUAOI2KU";
+	public static final String FIREBASE_API_KEY = "";
 	
-	public static final String FIREBASE_NOTIFICATION_API = "https://fcm.googleapis.com/fcm/send";
+	public static final String FIREBASE_NOTIFICATION_API = "";
 	
 }
